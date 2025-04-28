316c9dd79f736a6cd6617daa5173ce5f90217f5f
DavronbekSattorov <davronbekvssattorov@gmail.com>
2025-04-28 20:50:18 +0200

    tabs ready in homepage

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,23 @@
+import { View, Text, StyleSheet } from 'react-native';
+export function ProfileScreen() {
+    return (
+      <View style={styles.screen}><Text>Profile Screen</Text></View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    screen: {
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center',
+    },
+    notificationDot: {
+      position: 'absolute',
+      top: -2,
+      right: -6,
+      width: 8,
+      height: 8,
+      borderRadius: 4,
+      backgroundColor: 'red',
+    },
+  });
