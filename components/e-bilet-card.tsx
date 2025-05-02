df69d0c9223a2856267d104c9a5aa9ab028d050c
DavronbekSattorov <davronbekvssattorov@gmail.com>
2025-05-02 11:07:31 +0200

    two circle added to e-bilet-card

--- components/e-bilet-card.tsx
+++ components/e-bilet-card.tsx
@@ -12,7 +12,11 @@ export default function EBiletCard({
   return (
     <View style={styles.card}>
       {upperContent}
-      <View style={styles.divider} />
+      <View style={styles.dividerContainer}>
+        <View style={styles.notchLeft} />
+        <View style={styles.dashedLine} />
+        <View style={styles.notchRight} />
+      </View>
       {lowerContent}
     </View>
   );
@@ -25,11 +29,37 @@ const styles = StyleSheet.create({
     width: "90%",
     padding: 16,
   },
-
-  divider: {
-    borderBottomWidth: 1,
-    borderColor: "#ccc",
-    borderStyle: "dashed",
+  dividerContainer: {
+    width: "100%",
+    height: 40, // enough space for notches
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
     marginVertical: 20,
+  },
+  dashedLine: {
+    width: "100%",
+    borderBottomWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "#6C78E6", // dashed line color
+    backgroundColor: "transparent",
+    position: "absolute",
+    top: "50%",
+  },
+  notchLeft: {
+    position: "absolute",
+    left: -35,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#6C78E6",
+  },
+  notchRight: {
+    position: "absolute",
+    right: -35,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "#6C78E6",
   },
 });
